9ad1bad773c60ee1651126dea7c34183b2e30887
matias <peluche@Peluches-MBP.fibertel.com.ar>
2024-05-24 09:59:24 -0300

    coloco los prompts como variables de entorno

--- src/pages/inicio/home.jsx
+++ src/pages/inicio/home.jsx
@@ -6,25 +6,14 @@ import { Button, Typography } from "@material-tailwind/react";
 import React, { useEffect, useRef, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { marked } from "marked";
-//import dotnev from "dotenv";
-//dotenv.config();
 import "./ToggleButton.css";
+
 
 export function Home() {
 
-  const prompt_comprador = "Actúa como si fueses un Comprador que está interesado en celulares, vives en Argentina por lo que la moneda es pesos argentinos.\
-  Comenzaré el dialogo saludando, debes consultarme sobre lo que necesitas comprar, yo contestare la consulta como vendedor.\
-  Tu debes actuar siempre como un comprador interesado por un celular. Te avisaré cuando la conversación finalice.\
-  Debes actuar como un comprador durante toda la conversación, y si el vendedor se va de tema debes pedirle que se limite a vender celulares.\
-  \
-  Hola! En qué puedo ayudarte?";
+  const prompt_comprador = import.meta.env.VITE_PROMPT_COMPRADOR;
 
-  const prompt_vendedor = "actua como si fueses un vendedor de la empresa “retail-next” que esta interesado en vender típico de un retail. Interactua haciendo las consultas técnicas necesarias para poder vender un celular. las marcas que vendemos son samsung, apple, motorola y xiaomi. Siempre lo prioritario es ofrecer Samsung. el celular Apple es la ultima que la empresa prefiere vender. \
-  Cuando interactues quiero que le preguntes al usuario para que la necesita para entender la capacidad del celular, recomendale un celular acorde a lo que necesita, no hagas que compre un celular caro si no lo necesita. Si te das cuenta que el usuario esta consultando por un producto que no es de “retail-next” necesito que contestes “No es un producto de retail-next”. \
-  Cuando consultes no hace falta que expliques todo, simplemente limítate a hacer la consulta. el sitio web para comprar es: www.mercadolibre.com.ar, para obtener el link al producto concatenale al final una barra y luego el modelo. ofrecele primero comprar por la pagina, si vive en buenos aires el envio es gratis, caso contrario debe pagar 10.000 pesos. tambien se puede acercar a las sucursales que estan en www.google.com/maps/search/retail-next \
-  fijate de recordarle que tenemos las siguientes promos bancarias: \
-  6 cuotas sin interes con banco mariva \
-  30 % en un pago con banco galicia";
+  const prompt_vendedor = import.meta.env.VITE_PROMPT_VENDEDOR;
 
 
   const [mensajesEnviados, setMensajesEnviados] = useState([]);
